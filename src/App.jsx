4a54884752a0e32664f01b31ac7bb9a5a53ee0ba
CodeWithYashp <ue205131.yash.ece@gmail.com>
2023-10-25 15:54:43 +0530

    fixed reload bug using hasrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import { fetchDataFromApi } from "./utils/api";
 import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration, getGenres } from "./store/homeSlice";
